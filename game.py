44d356aa6e399559439d7cdcd3bff11c6c8ba036
ybjv02 <88575461+ybjv02@users.noreply.github.com>
2022-01-08 19:08:41 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,59 +1,61 @@
 # Rules: Shift + X to quit
-# .txt cotent: name,6.78 (no spaces)
+
+import sys
+import time
+import msvcrt
+import random
 
 
 def preamble():
-    \"\"\"Preamble for rhythm game\"\"\"
+    """Preamble for rhythm game"""
     for i in range(3, 0, -1):
         print(i)
         time.sleep(0.5)
-    print(\"Go!\")
+    print("Go!")
 
 
 def game():
-    \"\"\"Main function for rhythm game\"\"\"
+    """Main function for rhythm game"""
     notes = 20
     count = 0
     start = time.perf_counter()
-    mapped_keys = {\"d\": 1, \"f\": 2, \"j\": 3, \"k\": 4}
+    mapped_keys = {"d": 1, "f": 2, "j": 3, "k": 4}
     for i in range(notes):
-        s = \"| | | | |\"
+        s = "| | | | |"
         music_note = random.randint(1, 4)
         index = music_note * 2 - 1
         s = list(s)
-        s[index] = \"o\"
-        s = \"\".join(s)
+        s[index] = "o"
+        s = "".join(s)
         print(s)
         key = msvcrt.getch().decode()
         if key in mapped_keys and mapped_keys[key] == music_note:
             count += 1
-        elif key == \"X\":
-            print(\"User Quit.\")
+        elif key == "X":
+            print("User Quit.")
             exit(0) 
 
     end = time.perf_counter()
     accuracy = (count / notes) * 100
     time_taken = round(end - start, 2)
-    print(f\"\"\"
-Time Taken: {time_taken}s
-Accuracy: {accuracy}%\"\"\")
+    print(f"""Time Taken: {time_taken}\nAccuracy: {accuracy}%""")
     if accuracy == 100:
-        with open(\"record.txt\") as in_file:
-            name, record = in_file.readlines()[-1].split(\",\")
+        with open("record.txt") as in_file:
+            name, record = in_file.readlines()[-1].split(",")
             if time_taken < float(record):
-                print(f\"\"\"Congratulations, you beat {name} whose record was {float(record)}s!\"\"\")
-                name = input(\"Enter your name for the leaderboards: \")
-                with open(\"record.txt\") as out_file:
-                    out_file.write(f\"{name},{time_taken}\")
+                print(f"""Congratulations, you beat {name} whose record was {float(record)}s!""")
+                name = input("Enter your name for the leaderboards: ")
+                with open("record.txt", "w") as out_file:
+                    out_file.write(f"{name},{time_taken}")
 
 
 def main():
-    if len(sys.argv) == 2 and sys.argv[1] == \"- start\":
+    if len(sys.argv) == 2 and sys.argv[1] == "-start":
         preamble()
         game()
     else:
-        print(\"Usage: python rhythm_game.py -start\")
+        print("Usage: python rhythm_game.py -start")
 
 
-if __name__ == \"__main__\":
-    main()"
+if __name__ == "__main__":
+    main()
